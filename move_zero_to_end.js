83575d1d20141d7cc87318df33b139370a23a33c
M Danial <m.danial465@gmail.com>
2024-03-05 00:00:15 +0500

    Eleminating extra lines

--- move_zero_to_end.js
+++ move_zero_to_end.js
@@ -1,23 +1,23 @@
-let arr = [1,0,4,0,3,0,2];
+let arr = [1,0,4,0,3,0,2,7,4,0,5,0];
 
 function moveZeros(arr)
 {
     let zeroCount = 0;
     for(let i = 0; i < arr.length; i++)
     {
-        
-        let isZero = arr[i] === 0;
-        if(isZero)
-        {
-            zeroCount++;
-            arr.splice(i,1);
-            i--;
-        }
+        arr[i] === 0 && arr.splice(i,1) && arr.push(0);
+        // let isZero = arr[i] === 0;
+        // if(isZero)
+        // {
+        //     zeroCount++;
+        //     arr.splice(i,1);
+        //     i--;
+        // }
     }
-    for(let i = 0; i < zeroCount; i++)
-    {
-        arr.push(0);
-    }
+    // for(let i = 0; i < zeroCount; i++)
+    // {
+    //     arr.push(0);
+    // }
     
     return arr;
 }
